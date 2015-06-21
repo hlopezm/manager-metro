738ed0f03b09ddb54e6eb4596f2727f957aacc99
hlopez <hlopezm23@gmail.com>
2015-06-20 22:58:47 -0500

    Upload friend images via Paperclip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,5 +50,13 @@ Rails.application.configure do
     user_name: "hlopezm23@gmail.com",
     password: "android2"
   }
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 
 end
